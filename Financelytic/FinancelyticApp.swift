c42507a290e9432e0412de2ab4548659166bdb61
Neikkie <neikkie21@outlook.com>
2026-05-06 10:01:29 -0400

    Initial Commit

---
+++ Financelytic/FinancelyticApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancelyticApp.swift
+//  Financelytic
+//
+//  Created by Shanique Beckford on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FinancelyticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
